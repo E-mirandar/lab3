1e28ee88b50b86ac0f4a610df1ab4537f7077b02
E-mirandar <emirandaramirez@ucsd.edu>
2023-05-03 19:56:50 -0700

    bruh

--- ArrayTests.java
+++ ArrayTests.java
@@ -2,17 +2,36 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ArrayTests {
-	@Test 
+	// reversed in place test
+  @Test 
 	public void testReverseInPlace() {
-    int[] input1 = { 3 };
+    int[] input1 = {1, 2, 3, 4,5 };
+    int[] input2 = {1,2,3,4};
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
-	}
+    assertArrayEquals(new int[]{5,4,3,2,1}, input1); // passes
+    assertArrayEquals(new int[]{1,2,3,4}, input2);
+  }
 
-
+  // reversedonly test
   @Test
   public void testReversed() {
-    int[] input1 = { };
+    int[] input1 = { }; // passes
+    int[] input2 = {1, 2, 3, 4}; // passes with even
+    int[] input3 = {1,2,3,4,5};
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    assertArrayEquals(new int[]{4,3,2,1}, ArrayExamples.reversed(input2));
+    assertArrayEquals(new int[]{5,4,3,2,1}, ArrayExamples.reversed(input3));
+  }
+   
+  // average Without lowest tets 
+  // uses assert equals insted of assertARRAYEquals
+  public void testAverageWithoutLowest() { 
+    double[] nothing = { };
+    double[] oneItem = { 1.0 };
+    double[] output = {0.0};
+    assertEquals( output, ArrayExamples.averageWithoutLowest(nothing));
+    assertEquals( output, ArrayExamples.averageWithoutLowest(oneItem));
+
+     
   }
 }
--- MethodsTests.java
+++ MethodsTests.java
@@ -17,4 +17,18 @@ public class MethodsTests {
     int[] input1 = { 12, 13, 7, 8, 5, 3};
     assertEquals(EvensExample.sumEvenIndices(input1), 24);
   }
+
+  @Test
+  public void testReverse2() { 
+    int[] input = {1,2,3,4,5};
+    int[] output = ArrayExamples.reversed(input);
+    assertArrayEquals(new int[]{5,4,3,2,1}, output);
+    
+  }
+
+  @Test
+  public void testReversed() { 
+    int[] input1 = {};
+    assertArrayEquals(new int[]{}, ArrayExamples.reversed(input1));
+  }
 }
